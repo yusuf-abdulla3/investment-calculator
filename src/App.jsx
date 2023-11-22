063cce3a1b7fa1c6a3463655de88bfba46cc81f8
Yusuf <y.abdulla37@gmail.com>
2023-11-21 17:20:47 -0700

    finished adding results, header and user input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const inputIsValid = userInput.duration >=1;
+
+  function handleChange(inputIdentifier, newValue){
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue
+      }
+    })
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />  
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero. </p>}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+)
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/investment-calculator-logo.png';
+
+export default function Header () {
+  return(
+    <header id="header">
+      <img src={logo} alt="logo showing a money bag" />
+      <h1>Investment Calculator</h1>
+    </header>
+  )
+
+}
